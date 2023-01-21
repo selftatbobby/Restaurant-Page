27aa8c705449c5a90ffa551aed8bd1ff189a69cb
Bobby Tat <messaroundacc1@hotmail.com>
2023-01-21 05:38:10 -0800

    On step 5 of the project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-console.log('hello world')
-console.log('hella worldd')
+
+console.log('headsfld')
+console.log('helldd')
 function sayCussWord() {
     console.log("bitch")
 }
 
 sayCussWord();
+
+// let header = document.createElement("p");
+// let content = document.getElementById("content");
+// header.innerText = 'hi';
+// content.append(header); 
+// console.log(content)
+// console.log(header)
+
+

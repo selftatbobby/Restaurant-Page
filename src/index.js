b0d967bc745591c97535af98a092008384826c50
Bobby Tat <messaroundacc1@hotmail.com>
2023-02-09 05:42:38 -0800

    Working on tab switching logic to display correct content

--- src/index.js
+++ src/index.js
@@ -6,13 +6,15 @@ import { pageLoad, contentElement, headerElement, buttonElement,
 pageLoad();
 buttonElement.classList.add("active");
 
-function displayReset(target) {
+function displayReset(target, index) {
     var i, tablinks;  
+
+    //Set all tabcontent classed elements' display to none
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
     
-    //Removes 'active' class from all buttons with class 'tablinks'
+    //Removes 'active' class from all buttons with 'tablinks' class 
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
@@ -20,14 +22,19 @@ function displayReset(target) {
 
     //Adds 'active' class to current target button
     target.currentTarget.className += " active";
+    console.log(target.currentTarget);
 
     //Shows the selected tab's content
+    // tabcontent[index].style.display = "block";
+    // console.log(tabcontent[0]);
 
 }
 
 
-buttonElement.addEventListener("click", displayReset);
+// buttonElement.addEventListener("click", displayReset);
+buttonElement.onclick = displayReset;
 buttonElement1.addEventListener("click", displayReset);
 buttonElement2.addEventListener("click", displayReset);
+
 
 

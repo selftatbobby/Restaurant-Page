cf33db06f1a56677b7413d640c6f94a2c3a8d333
Bobby Tat <messaroundacc1@hotmail.com>
2023-02-15 05:33:36 -0800

    I will try separating the functions to complete the logic for tab switching

--- src/index.js
+++ src/index.js
@@ -6,37 +6,64 @@ import { pageLoad, contentElement, headerElement, buttonElement,
 pageLoad();
 buttonElement.classList.add("active");
 
-function displayReset(e, index) {
+function displayReset(index) {
     var i, tablinks;  
 
     //Set all tabcontent classed elements' display to none
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
+    // for (i = 0; i < tabcontent.length; i++) {
+    //   tabcontent[i].style.display = "none";
+    // }
     
     //Removes 'active' class from all buttons with 'tablinks' class 
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+    // tablinks = document.getElementsByClassName("tablinks");
+    // for (i = 0; i < tablinks.length; i++) {
+    //     tablinks[i].className = tablinks[i].className.replace(" active", "");
+    // }
 
     //Adds 'active' class to current target button
     //e.currentTarget is only available whle the event is being handled
-    e.currentTarget.className += " active";
-    console.log(e.currentTarget);
-    console.log(this);
+    // e.target.className += " active";
+    // console.log(e.currenTarget);
+    // this.className += " active";
+    // console.log(this);
 
     //Shows the selected tab's content
     // tabcontent[index].style.display = "block";
-    // console.log(tabcontent[0]);
+    // console.log(tabcontent[index]);
 
 }
 
+//highlights selected tab, dehighlights others, and hides all tab content
+function currentTab(e) {
+    var i, tablinks;
 
+    //Set all tabcontent classed elements' display to none
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+
+    //Removes 'active' class from all buttons with 'tablinks' class   
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+      }
+
+    //Adds 'active' class to current target button  
+    e.currentTarget.className += " active";
+    console.log(e.currentTarget);
+
+ 
+
+}
+
+buttonElement.addEventListener("click", currentTab);
+buttonElement1.addEventListener("click", currentTab);
+buttonElement2.addEventListener("click", currentTab);
+
+// buttonElement.onclick = displayReset;
 // buttonElement.addEventListener("click", displayReset);
-buttonElement.onclick = displayReset;
-buttonElement1.addEventListener("click", displayReset);
-buttonElement2.addEventListener("click", displayReset);
+// buttonElement1.addEventListener("click", displayReset);
+// buttonElement2.addEventListener("click", displayReset);
 
 
 

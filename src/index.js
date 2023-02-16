5f4a96f688ec6a2462850a379fb144a18aacdd9b
Bobby Tat <messaroundacc1@hotmail.com>
2023-02-16 05:54:51 -0800

    Created two new modules for tab content. Tab switching logic is a work in progress

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import "./style.css";
 import { pageLoad, contentElement, headerElement, buttonElement, 
-    buttonElement1, buttonElement2, mainElement, imageElement, sloganElement, tabcontent } from './page-load';
+    buttonElement1, buttonElement2, mainElement, imageElement, sloganElement, tabcontent } from "./page-load";
+import { menuLoad, menuElement, menuContent } from "./menu-load";
 
 
 pageLoad();
+menuLoad();
+
 buttonElement.classList.add("active");
 
-function displayReset(index) {
-    var i, tablinks;  
+function displayTab(index) {
+    var i;  
 
     //Set all tabcontent classed elements' display to none
-    // for (i = 0; i < tabcontent.length; i++) {
-    //   tabcontent[i].style.display = "none";
-    // }
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
     
     //Removes 'active' class from all buttons with 'tablinks' class 
     // tablinks = document.getElementsByClassName("tablinks");
@@ -28,8 +31,8 @@ function displayReset(index) {
     // console.log(this);
 
     //Shows the selected tab's content
-    // tabcontent[index].style.display = "block";
-    // console.log(tabcontent[index]);
+    tabcontent[index].style.display = "block";
+    console.log(tabcontent[index]);
 
 }
 
@@ -38,9 +41,9 @@ function currentTab(e) {
     var i, tablinks;
 
     //Set all tabcontent classed elements' display to none
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-      }
+    // for (i = 0; i < tabcontent.length; i++) {
+    //     tabcontent[i].style.display = "none";
+    //   }
 
     //Removes 'active' class from all buttons with 'tablinks' class   
     tablinks = document.getElementsByClassName("tablinks");
@@ -50,14 +53,21 @@ function currentTab(e) {
 
     //Adds 'active' class to current target button  
     e.currentTarget.className += " active";
-    console.log(e.currentTarget);
+    // console.log(e.currentTarget);
 
  
 
 }
+// console.log(tabcontent[0]);
+// console.log(tabcontent[1]);
+
 
 buttonElement.addEventListener("click", currentTab);
+buttonElement.addEventListener("click", displayTab(0))
+
 buttonElement1.addEventListener("click", currentTab);
+buttonElement1.addEventListener("click", displayTab(1));
+
 buttonElement2.addEventListener("click", currentTab);
 
 // buttonElement.onclick = displayReset;
---
+++ src/menu-load.js
@@ -0,0 +1,15 @@
+import { contentElement } from "./page-load";
+let menuElement = document.createElement("div");
+let menuContent = document.createElement("p")
+
+const menuLoad = () => {
+    //Add menu elements to contentElement
+    contentElement.appendChild(menuElement);
+    menuElement.classList.add("tabcontent");
+
+    menuContent.innerHTML = "The Menu";
+    menuElement.appendChild(menuContent);
+    
+}
+
+export { menuLoad, contentElement, menuElement, menuContent };
